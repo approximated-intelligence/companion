80c4b21250e0c532949edaaa14a7a6de721737b9
Christian Bahls <asm2amqp@qb352.de>
2026-04-20 19:22:36 +0500

    Update AudioRecordingScreen.kt, TextOverflow

--- app/src/main/kotlin/de/perigon/companion/audio/ui/recording/AudioRecordingScreen.kt
+++ app/src/main/kotlin/de/perigon/companion/audio/ui/recording/AudioRecordingScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
